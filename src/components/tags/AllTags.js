9b70067a8b6e5f870fa878fdba6fd91cc04ebb92
Henry House <hankhaus99@gmail.com>
2022-04-21 10:17:38 -0500

    comments for view tag list

--- src/components/tags/AllTags.js
+++ src/components/tags/AllTags.js
@@ -1,1 +1,4 @@
 // basically copy the "allPosts.js" module but refactor for getting tags 
+
+    // add edit and delete buttons within the Map, so that each tag has an edit and delete button, 
+    // don't worry about functionality for buttons yet
