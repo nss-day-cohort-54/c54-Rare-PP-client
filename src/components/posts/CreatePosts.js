40aecccf6192df3637fe560029a23db7b2dc44bd
Henry House <hankhaus99@gmail.com>
2022-04-22 09:59:31 -0500

    add tags to post comments

--- src/components/posts/CreatePosts.js
+++ src/components/posts/CreatePosts.js
@@ -6,7 +6,10 @@ export const CreatePosts = () => {
 
 
 
-
+// invoke getAllTags from TagManager.js, and set all the tags to a variable "tags" with useState
+    // map through the list of all tags, displaying a checkbox for each tag along with the name/label for each tag
+    // comments for creating a post have yet to be written, but when the user selects at least one tag, attach it 
+    // to the post that is getting sent to the db
 
 
     return <div>CreatePosts Page</div>
