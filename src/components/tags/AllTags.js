489b307b2d54848db231acac18e0872406516c4d
Henry House <hankhaus99@gmail.com>
2022-04-22 15:21:43 -0500

    add new tag form working

--- src/components/tags/AllTags.js
+++ src/components/tags/AllTags.js
@@ -1,6 +1,6 @@
 import { getAllTags } from "./TagManager"
 import React, { useEffect, useState } from "react";
-
+import { NewTagForm } from "./CreateTagForm";
 export const AllTags = () => {
 
     const [tags, setTags] = useState([])
@@ -14,6 +14,9 @@ export const AllTags = () => {
         [])
     return <>
         <div>AllTags Page</div>
+        <div className="CreateNewTagFormContainer">
+            <NewTagForm />
+        </div>
         {tags.map((tag) => {
             return <div key={`tag--${tag.id}`}>{tag.label} 
             <button>edit</button> <button>delete</button>
