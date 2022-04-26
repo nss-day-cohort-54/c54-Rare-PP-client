93f9a525048540bd87549594e1a473b5855917a8
Matthew McMorries <matthew.mcmorries@gmail.com>
2022-04-26 16:42:23 -0500

    computer nap

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -36,7 +36,7 @@ export const ApplicationViews = () => {
       <Route exact path="/posts/single/:postId(\d+)">
         <SinglePost />
       </Route>
-      
+
       {/* <Route exact path="/posts/myPosts">
       <Route exact path="/posts/myPosts">
         <MyPosts />
