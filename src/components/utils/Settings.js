2cc99191f7e1a1b6f3fe40edd5be965c539cfbdf
David Clark <david.clark.1043@gmail.com>
2022-04-27 11:42:49 -0500

    Updated links to icon images

--- src/components/utils/Settings.js
+++ src/components/utils/Settings.js
@@ -1,3 +1,5 @@
 export const Settings = {
-    API: "http://localhost:8088"
+    API: "http://localhost:8088",
+    EditIcon: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Edit_icon_%28the_Noun_Project_30184%29.svg/1024px-Edit_icon_%28the_Noun_Project_30184%29.svg.png",
+    DeleteIcon: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Font_Awesome_5_regular_trash-alt.svg/512px-Font_Awesome_5_regular_trash-alt.svg.png"
 }
